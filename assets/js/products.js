e3deedb0b7db12e5057529419607d03f8b261772
valaryuka <valaryuka@gmail.com>
2021-10-28 22:11:52 -0300

    products 1.1

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,24 +1,6 @@
+function delProduct(id) {
 
-$(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();
-	
-	// Select/Deselect checkboxes
-	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
-		if(this.checked){
-			checkbox.each(function(){
-				this.checked = true;                        
-			});
-		} else{
-			checkbox.each(function(){
-				this.checked = false;                        
-			});
-		} 
-	});
-	checkbox.click(function(){
-		if(!this.checked){
-			$("#selectAll").prop("checked", false);
-		}
-	});
-});
+}
+
+$('#myDiv').click(function(){ //Some code 
+}); 
