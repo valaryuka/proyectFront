1419f57ecb5590e6b61e9cf92b5ab666ec714f36
Goner01 <omarjesusleivasanchez18@gmail.com>
2021-10-28 22:20:20 -0300

    se agrega inicio sesión

---
+++ assets/js/user.js
@@ -0,0 +1,26 @@
+const API_URL = "http://localhost:3000/signIn";
+const button = document.getElementById('signIn');
+
+button.addEventListener('click', event => {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+
+    axios.post(
+        API_URL, {email, pass},
+        )
+        .then(response => {
+            console.log(response.data); // response.data ya es un JSON
+            location.href = 'D:/Documentos/NTTDATA desafio/proyectFront/products.html';
+        }).catch(err => {
+            
+            const msgError = document.getElementById('msg-error');
+            msgError.innerHTML = `<p class="text-danger">${err.response.data.error}</p>`;
+            setTimeout(()=>{
+                msgError.innerHTML = `<p></p>`;
+            },3000);     
+    });   
+
+});
+
